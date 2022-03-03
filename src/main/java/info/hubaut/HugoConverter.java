5c80b6bc36d175e2b03a689a79013563998139ae
Olivier Hubaut <olivier@hubaut.info>
2022-03-03 19:09:15 +0100

    Basic conversion and minimal metadata extraction.

--- src/main/java/info/hubaut/HugoConverter.java
+++ src/main/java/info/hubaut/HugoConverter.java
@@ -1,12 +1,25 @@
 package info.hubaut;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+import org.snakeyaml.engine.v2.api.Dump;
+import org.snakeyaml.engine.v2.api.DumpSettings;
+import org.snakeyaml.engine.v2.common.FlowStyle;
+
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 import java.nio.file.FileVisitResult;
 import java.nio.file.FileVisitor;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.StandardCopyOption;
+import java.nio.file.StandardOpenOption;
 import java.nio.file.attribute.BasicFileAttributes;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class HugoConverter implements FileVisitor<Path> {
@@ -14,10 +27,15 @@ public class HugoConverter implements FileVisitor<Path> {
     private final Path baseDir;
     private final Path output;
     private final Map<String, Path> folders = new HashMap<>();
+    private final Dump yamlDump;
 
     public HugoConverter(Path baseDir, Path output) {
         this.baseDir = baseDir;
         this.output = output;
+        DumpSettings dumpSettings = DumpSettings.builder()
+                                                .setDefaultFlowStyle(FlowStyle.BLOCK)
+                                                .build();
+        yamlDump = new Dump(dumpSettings);
     }
 
     public void convert() throws IOException {
@@ -32,6 +50,13 @@ public class HugoConverter implements FileVisitor<Path> {
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         Path outputFolder = getOutputFolder(file);
+        if (file.toFile()
+                .getName()
+                .matches(".*\\.htm(l?)")) {
+            convertContent(file, outputFolder);
+        } else {
+            transfer(file, outputFolder);
+        }
         return FileVisitResult.CONTINUE;
     }
 
@@ -44,22 +69,66 @@ public class HugoConverter implements FileVisitor<Path> {
     @Override
     public FileVisitResult postVisitDirectory(Path dir, IOException exc) {
         return exc != null
-               ? FileVisitResult.TERMINATE
-               : FileVisitResult.CONTINUE;
+                ? FileVisitResult.TERMINATE
+                : FileVisitResult.CONTINUE;
+    }
+
+    private void transfer(Path file, Path outputFolder) throws IOException {
+        Files.copy(Files.newInputStream(file), outputFolder.resolve(file.getFileName()), StandardCopyOption.REPLACE_EXISTING);
+    }
+
+    private void convertContent(Path file, Path outputFolder) throws IOException {
+        Document document = Jsoup.parse(Files.newInputStream(file), StandardCharsets.UTF_8.name(), "");
+        String frontMatter = generateFrontMatter(document);
+        Elements elements = document.select(".central > .texte");
+        if (elements.isEmpty()) {
+            System.out.println("No main content for " + file);
+        } else if (elements.size() > 1) {
+            System.out.println("More than one main content for " + file);
+        } else {
+            Element content = elements.first();
+            Path outputPath = computeOutputPath(file, outputFolder);
+            try (BufferedWriter writer = Files.newBufferedWriter(outputPath, StandardOpenOption.CREATE,
+                    StandardOpenOption.TRUNCATE_EXISTING)) {
+                writer.append("---");
+                writer.newLine();
+                writer.append(frontMatter);
+                writer.append("---");
+                writer.newLine();
+                writer.append(content.html());
+            }
+        }
+    }
+
+    private String generateFrontMatter(Document document) {
+        List<Map<String, String>> metadata = document.head()
+                                                     .select("meta")
+                                                     .stream()
+                                                     .filter(element -> element.hasAttr("name"))
+                                                     .map(element -> Map.of("name", element.attr("name"), "value", element.attr("content")))
+                                                     .toList();
+        Map<String, Object> frontMatter = Map.of("title", document.title(), "meta", metadata);
+        return yamlDump.dumpToString(frontMatter);
+    }
+
+    private Path computeOutputPath(Path file, Path outputFolder) {
+        Path fileName = file.getFileName();
+        String originalName = fileName.toString();
+        String newName = originalName.substring(0, originalName.lastIndexOf(".")) + ".md";
+        return outputFolder.resolve(newName);
     }
 
     private Path getOutputFolder(Path file) {
         Path relativePath = baseDir.relativize(file);
         Path parentPath = relativePath.getParent();
         return parentPath == null
-               ? output
-               : folders.computeIfAbsent(parentPath.toString(), this::createOutputFolder);
+                ? output
+                : folders.computeIfAbsent(parentPath.toString(), this::createOutputFolder);
     }
 
     private Path createOutputFolder(String relativeFolder) {
         Path outputFolder = output.resolve(relativeFolder);
         try {
-            System.out.println("Creating folder " + outputFolder);
             Files.createDirectories(outputFolder);
         } catch (IOException e) {
             throw new RuntimeException(e);
--- src/main/java/info/hubaut/SiteConverter.java
+++ src/main/java/info/hubaut/SiteConverter.java
@@ -6,16 +6,20 @@ import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.Instant;
 
 public class SiteConverter {
     public static void main(String[] args) throws URISyntaxException, IOException {
+        Instant before = Instant.now();
         System.out.println("Starting conversion");
         SiteConverter converter = new SiteConverter();
         URL inputURL = SiteConverter.class.getResource("/original/coursmath");
         Path inputPath = Paths.get(inputURL.toURI());
         Path output = Files.createDirectories(Path.of("site", "content"));
         converter.convertToHugo(inputPath, output);
-        System.out.println("Conversion finished");
+        Instant after = Instant.now();
+        System.out.println("Conversion finished in " + Duration.between(before, after));
     }
 
     private void convertToHugo(Path input, Path output) throws IOException {
